8be37dd6906a3eda1de7d893ce60df01b627f058
pearl-lee <93pearl.lee@gmail.com>
2020-07-11 20:26:21 +0900

    draw square with turtle

--- python/turtle_square.py
+++ python/turtle_square.py
@@ -7,11 +7,20 @@ tt.color('green')
 tt.pensize(5)
 
 tt.forward(100) # 기본값인 오른쪽으로 100만큼 이동
-tt.left(90) # 왼쪽으로 꺾기
+tt.right(90) # 왼쪽으로 꺾기
 tt.forward(100)
 tt.left(90)
 tt.forward(100)
 tt.left(90)
+tt.forward(300)
+tt.left(90)
 tt.forward(100)
+tt.left(90)
+tt.forward(100)
+tt.right(90)
+tt.forward(100)
+tt.left(90)
+tt.forward(100)
+# 직각주차장 ㅠ_ㅠ
 
 time.sleep(5)
